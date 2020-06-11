3d0a3dd133408c2779c6770b807475ef1acff4cc
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-11 23:51:55 +0530

    ADDED : function for resetting board

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -x
 echo " Welcome to the Tic Tac Toe Game"
+
+NUM_OF_ROWS=3
+NO_OF_COLUMNS=3
+EMPTY=-1
+
+declare -a dictBoard
+
+function resetBoard(){
+for ((i=0; i<NUM_OF_ROWS; i++))
+	do
+		for ((j=0; j<NUM_OF_COLUMNS; J++))
+			do
+				echo "dictBoard[$i,$j]=EMPTY"
+			done
+	done
+}
+resetBoard
